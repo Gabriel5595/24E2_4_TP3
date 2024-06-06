00ea5835e221658f430ed7761a6006d00ad29d77
Gabriel5595 <gabriel.vcarvalho@al.infnet.edu.br>
2024-06-06 18:26:18 +0200

    Finalização do script_2

---
+++ components/script_2.py
@@ -0,0 +1,18 @@
+from components.load_data import load_data
+from components.save_json_file import save_json_file
+from components.search_name import search_name
+
+def script_2():
+    file_csv = "C:/Users/gribe/OneDrive/Documentos/Codes/INFNET/2024.1/Python para Dados/TPs/TP3/resources/database.csv"
+    df = load_data(file_csv)
+
+    print("Parte 1:")
+    print("Realize uma consulta por um nome salvo em um objeto json.")
+    name = "Ana"
+    search_result = search_name(df, name, json_flag=True)
+    print(search_result)
+    print()
+    
+    print("Parte 2:")
+    print("Realize o salvamento do objeto json em um arquivo .json.")
+    save_json_file("search_result", search_result)
---
+++ test/script_2.py
@@ -0,0 +1,28 @@
+import sys
+import os
+# Adiciona o diretório raiz do projeto ao sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from components.load_data import load_data
+from components.save_json_file import save_json_file
+from components.search_name import search_name
+
+def script_2():
+    file_csv = "C:/Users/gribe/OneDrive/Documentos/Codes/INFNET/2024.1/Python para Dados/TPs/TP3/resources/database.csv"
+    df = load_data(file_csv)
+
+    print("Parte 1:")
+    print("Realize uma consulta por um nome salvo em um objeto json.")
+    name = "Ana"
+    search_result = search_name(df, name, json_flag=True)
+    print(search_result)
+    print()
+    
+    print("Parte 2:")
+    print("Realize o salvamento do objeto json em um arquivo .json.")
+    avg_age = save_json_file("search_result", search_result)
+
+def main():
+    script_2()
+
+main()
