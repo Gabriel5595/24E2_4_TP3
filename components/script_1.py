2ab20ff3834a609bcdb8c0a8b32d1d74ee3b5182
Gabriel5595 <gabriel.vcarvalho@al.infnet.edu.br>
2024-06-06 18:18:09 +0200

    Finalização do script_1

---
+++ components/script_1.py
@@ -0,0 +1,27 @@
+from components.load_data import load_data
+from components.calculate_avg_age import calculate_avg_age
+from components.calculate_avg_children import calculate_avg_children
+from components.search_name import search_name
+
+def script_1():
+    file_csv = "C:/Users/gribe/OneDrive/Documentos/Codes/INFNET/2024.1/Python para Dados/TPs/TP3/resources/database.csv"
+    df = load_data(file_csv)
+
+    print("Parte 1:")
+    print("Realize uma consulta por um nome salvo em um dataframe pandas.")
+    name = "Ana"
+    search_result = search_name(df, name)
+    print(search_result)
+    print()
+    
+    print("Parte 2:")
+    print("Exiba a média de idade para o dataframe recebido.")
+    avg_age = calculate_avg_age(df)
+    print(f"Média de idade: {avg_age}")
+    print()
+
+    print("Parte 3:")
+    print("Exiba a média do número de filhos para o dataframe recebido.")
+    avg_children = calculate_avg_children(df)
+    print(f"Média do número de filhos: {avg_children}")
+    print()
